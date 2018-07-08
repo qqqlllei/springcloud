5a61c12ed3566f9b62a585f0e04d612bd4ed1f35
qqlei <1091687748@qq.com>
2018-07-08 23:22:46 +0800

    init

--- gateway/src/main/java/com/qqlei/cloud/gateway/GateWayApplication.java
+++ gateway/src/main/java/com/qqlei/cloud/gateway/GateWayApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  * Created by Administrator on 2018/4/14 0014.
