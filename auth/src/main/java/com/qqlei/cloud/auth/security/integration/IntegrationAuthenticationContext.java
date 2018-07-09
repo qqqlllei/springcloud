3d6c0ee209ae32c9b7c3c7b3244ed339de029416
lilei <861971291@qq.com>
2018-07-09 18:52:48 +0800

    oauth123123

---
+++ auth/src/main/java/com/qqlei/cloud/auth/security/integration/IntegrationAuthenticationContext.java
@@ -0,0 +1,18 @@
+package com.qqlei.cloud.auth.security.integration;
+
+public class IntegrationAuthenticationContext {
+
+    private static ThreadLocal<IntegrationAuthentication> holder = new ThreadLocal<IntegrationAuthentication>();
+
+    public static void set(IntegrationAuthentication integrationAuthentication){
+        holder.set(integrationAuthentication);
+    }
+
+    public static IntegrationAuthentication get(){
+        return holder.get();
+    }
+
+    public static void clear(){
+        holder.remove();
+    }
+}
---
+++ auth/src/main/java/com/qqlei/cloud/auth/security/integration/authenticator/IntegrationAuthenticator.java
@@ -0,0 +1,35 @@
+package com.qqlei.cloud.auth.security.integration.authenticator;
+
+
+import com.qqlei.cloud.auth.security.integration.IntegrationAuthentication;
+import com.qqlei.cloud.auth.security.vo.SysUserAuthentication;
+
+public interface IntegrationAuthenticator {
+
+    /**
+     * 处理集成认证
+     * @param integrationAuthentication
+     * @return
+     */
+    SysUserAuthentication authenticate(IntegrationAuthentication integrationAuthentication);
+
+
+    /**
+     * 进行预处理
+     * @param integrationAuthentication
+     */
+    void prepare(IntegrationAuthentication integrationAuthentication);
+
+     /**
+     * 判断是否支持集成认证类型
+     * @param integrationAuthentication
+     * @return
+     */
+    boolean support(IntegrationAuthentication integrationAuthentication);
+
+    /** 认证结束后执行
+     * @param integrationAuthentication
+     */
+    void complete(IntegrationAuthentication integrationAuthentication);
+
+}
