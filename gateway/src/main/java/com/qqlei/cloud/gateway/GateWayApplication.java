3e6b6bc4c7595cd575414a0483656080ba862a02
lilei <861971291@qq.com>
2018-07-12 11:02:49 +0800

    oauth123123

--- gateway/src/main/java/com/qqlei/cloud/gateway/GateWayApplication.java
+++ gateway/src/main/java/com/qqlei/cloud/gateway/GateWayApplication.java
@@ -3,11 +3,8 @@ package com.qqlei.cloud.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 /**
  * Created by Administrator on 2018/4/14 0014.
@@ -15,6 +12,7 @@ import org.springframework.web.filter.CorsFilter;
 @SpringBootApplication
 @EnableZuulProxy
 @EnableOAuth2Sso
+@EnableDiscoveryClient
 public class GateWayApplication {
 
     public static void main(String[] args) {
--- gateway/src/main/java/com/qqlei/cloud/gateway/config/DynamicRouteConfiguration.java
+++ gateway/src/main/java/com/qqlei/cloud/gateway/config/DynamicRouteConfiguration.java
@@ -2,13 +2,12 @@ package com.qqlei.cloud.gateway.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.core.StringRedisTemplate;
+
 
 /**
  * Created by Administrator on 2018/7/11 0011.
