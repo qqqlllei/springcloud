0989a5fb8dadbe93e05376e76beeccb60515d0ff
lilei <861971291@qq.com>
2018-07-24 19:11:51 +0800

    oauth123123

---
+++ auth/src/main/java/com/qqlei/cloud/auth/security/integration/authenticator/dingding/DingDingIntegrationAuthenticator.java
@@ -0,0 +1,51 @@
+package com.qqlei.cloud.auth.security.integration.authenticator.dingding;
+
+import com.qqlei.cloud.auth.fegin.UserFegin;
+import com.qqlei.cloud.auth.security.dingding.DingTokenServer;
+import com.qqlei.cloud.auth.security.integration.IntegrationAuthentication;
+import com.qqlei.cloud.auth.security.integration.authenticator.IntegrationAuthenticator;
+import com.qqlei.cloud.auth.security.vo.SysUserAuthentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by 李雷 on 2018/7/24.
+ */
+@Service
+public class DingDingIntegrationAuthenticator implements IntegrationAuthenticator {
+
+    private static final String DING_DING_LOGIN_CODE_PARAM_NAME="code";
+
+
+    private final static String DING_DING_AUTH_TYPE = "dingding";
+
+    @Autowired
+    private DingTokenServer dingTokenServer;
+
+    @Autowired
+    private UserFegin userFegin;
+
+    @Override
+    public SysUserAuthentication authenticate(IntegrationAuthentication integrationAuthentication) {
+        String phone = integrationAuthentication.getUsername();
+        SysUserAuthentication sysUserAuthentication = userFegin.findUserByPhoneNumber(phone);
+        return sysUserAuthentication;
+    }
+
+    @Override
+    public void prepare(IntegrationAuthentication integrationAuthentication) {
+        String code = integrationAuthentication.getAuthParameter(DING_DING_LOGIN_CODE_PARAM_NAME);
+        String phone = dingTokenServer.getUserPhoneByAuthCode(code);
+        integrationAuthentication.setUsername(phone);
+    }
+
+    @Override
+    public boolean support(IntegrationAuthentication integrationAuthentication) {
+        return DING_DING_AUTH_TYPE.equals(integrationAuthentication.getAuthType());
+    }
+
+    @Override
+    public void complete(IntegrationAuthentication integrationAuthentication) {
+
+    }
+}
