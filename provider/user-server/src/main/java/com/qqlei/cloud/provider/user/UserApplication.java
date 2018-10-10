c0a30415ba5403c738e021ce3e84186d9642bb92
lilei <861971291@qq.com>
2018-10-10 20:05:02 +0800

    整合微信公众号登陆

--- provider/user-server/src/main/java/com/qqlei/cloud/provider/user/UserApplication.java
+++ provider/user-server/src/main/java/com/qqlei/cloud/provider/user/UserApplication.java
@@ -1,11 +1,13 @@
 package com.qqlei.cloud.provider.user;
 
+import com.reliable.message.client.job.ClientMessageDataflow;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
